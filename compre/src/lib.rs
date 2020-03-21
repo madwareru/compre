61ec4af1f7cd221e89bef4003d4f12e29eef36ed
Vitaly <madware.ru@gmail.com>
2020-03-21 17:29:21 +0300

    Update testst

--- compre/src/lib.rs
+++ compre/src/lib.rs
@@ -114,6 +114,10 @@ mod test{
 
     #[test]
     fn test_simple() {
+        let expected = vec![
+            ( 3,  4,  5), ( 4,  3,  5), ( 5, 12, 13), ( 6,  8, 10), ( 8,  6, 10),
+            ( 8, 15, 17), ( 9, 12, 15), (12,  5, 13), (12,  9, 15), (15,  8, 17)];
+
         let tripples = compre! {
             (x, y, z);
             x <- 1..=17,
@@ -121,14 +125,7 @@ mod test{
             z <- 1..=17;
             x*x + y*y == z*z
         }.collect::<Vec<_>>();
-
-        assert_eq!(
-            vec![(3,  4,  5), ( 4,  3,  5), (5, 12, 13), ( 6,  8, 10),
-                 (8,  6, 10), ( 8, 15, 17), (9, 12, 15), (12,  5, 13),
-                 (12, 9, 15), (15,  8, 17)
-            ],
-            tripples
-        );
+        assert_eq!(&expected, &tripples);
 
         let tripples = hx_do! {
             x <- 1..=17,
@@ -137,14 +134,7 @@ mod test{
             barrier: x*x + y*y == z*z;
             (x, y, z)
         }.collect::<Vec<_>>();
-
-        assert_eq!(
-            vec![(3,  4,  5), ( 4,  3,  5), (5, 12, 13), ( 6,  8, 10),
-                 (8,  6, 10), ( 8, 15, 17), (9, 12, 15), (12,  5, 13),
-                 (12, 9, 15), (15,  8, 17)
-            ],
-            tripples
-        );
+        assert_eq!(&expected, &tripples);
 
         let tripples = monadde! {
             1..=17 => x |>
@@ -153,14 +143,7 @@ mod test{
             when x*x + y*y == z*z =>
             (x, y, z)
         }.collect::<Vec<_>>();
-
-        assert_eq!(
-            vec![(3,  4,  5), ( 4,  3,  5), (5, 12, 13), ( 6,  8, 10),
-                 (8,  6, 10), ( 8, 15, 17), (9, 12, 15), (12,  5, 13),
-                 (12, 9, 15), (15,  8, 17)
-            ],
-            tripples
-        );
+        assert_eq!(&expected, &tripples);
     }
 
     #[test]
